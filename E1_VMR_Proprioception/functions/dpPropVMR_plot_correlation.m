d5b0689afcb0df0bc556175281d402fc9beb0db3
Darius Parvin <darius@berkeley.edu>
2019-02-03 01:07:42 -0800

    Added reliability plots
    Added a table to calculate stats for day 1 results and day 2 results. Plots reliability for day 1 vs day 2.
    
    Cleaned up some plotting functions

--- E1_VMR_Proprioception/functions/dpPropVMR_plot_correlation.m
+++ E1_VMR_Proprioception/functions/dpPropVMR_plot_correlation.m
@@ -12,14 +12,14 @@ yData = data.(yVar{:});
 
 plot(xData, yData ,'.','markersize',20)
 
-[rho,pval] = corrcoef(xData,yData)
-r = rho(1,2)
-p = pval(1,2)
+[rho,pval] = corrcoef(xData,yData);
+r = rho(1,2);
+p = pval(1,2);
 
 xlabel(xVar)
 ylabel(yVar)
 
-str = sprintf('%s vs %s, r = %.2f, p = %.2f',xVar{:}, yVar{:}, r, p)
+str = sprintf('%s vs %s, r = %.2f, p = %.2f',xVar{:}, yVar{:}, r, p);
 title(str)
 
 end
