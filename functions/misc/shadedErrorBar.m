f2e8635bcd0dc5c0f42e345dc8fe2c9753bcb30f
Darius Parvin <41220998+DariusParvin@users.noreply.github.com>
2019-03-08 03:15:48 -0800

    edited shaded errorbar

--- functions/misc/shadedErrorBar.m
+++ functions/misc/shadedErrorBar.m
@@ -147,8 +147,8 @@ H.patch=patch(xP,yP,1,'facecolor',patchColor,...
 
 
 %Make pretty edges around the patch. 
-H.edge(1)=plot(x,lE,'-','color',edgeColor);
-H.edge(2)=plot(x,uE,'-','color',edgeColor);
+% H.edge(1)=plot(x,lE,'-','color',edgeColor);
+% H.edge(2)=plot(x,uE,'-','color',edgeColor);
 
 %Now replace the line (this avoids having to bugger about with z coordinates)
 delete(H.mainLine)
